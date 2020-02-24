50483b52fef3868e3eb8a0af49ec327db9ab58d3
emCog <robin.leonard@gmail.com>
2020-02-24 17:10:28 +0000

    changing source of js

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,4 +1,7 @@
-// play pause
+// play pause /*
+
+/*
+
 const playPauseBtn = document.getElementbyId('amplitude-play-pause');
 
 playPauseBtn.addEventListener('mousedown', playPause);
@@ -11,3 +14,73 @@ function playPause() {
 		this.classList.add('player_is-playing');
 	}
 }
+
+*/
+
+
+// ---- > From index.html
+
+
+Amplitude.init({
+		"songs": [
+		{
+			"name": "Three-step reset",
+			"artist": "Mark Leonard",
+			"album": "MBOE Meditations",
+			"url": "/assets/meditations/three-step-reset.mp3"
+		},
+		{
+			"name": "Bodyscan",
+			"artist": "Mark Leonard",
+			"album": "MBOE Meditations",
+			"url": "/assets/meditations/bodyscan.mp3"
+		},
+		{
+			"name": "Posture and breath",
+			"artist": "Mark Leonard",
+			"album": "MBOE Meditations",
+			"url": "/assets/meditations/posture-and-breath.mp3"
+		},
+		{
+			"name": "Stretching and sitting",
+			"artist": "Mark Leonard",
+			"album": "MBOE Meditations",
+			"url": "/assets/meditations/stretching-and-sitting.mp3"
+		},
+		{
+			"name": "Mindfulness of feelings",
+			"artist": "Mark Leonard",
+			"album": "MBOE",
+			"url": "/assets/meditations/mindfulness-of-feelings.mp3"
+		},
+		{
+			"name": "Concentration and allowing the mind to wander",
+			"artist": "Mark Leonard",
+			"album": "MBOE",
+			"url": "/assets/meditations/concentration-and-allowing-the-mind-to-wander.mp3"
+		}
+	],
+
+	continue_next: false,
+	debug: true
+
+	});
+	
+	// clickable slider
+	document.getElementById('song-played-progress').addEventListener('click', function(e){
+		let offset = this.getBoundingClientRect();
+		let x = e.pageX - offset.left;
+
+		Amplitude.setSongPlayedPercentage((parseFloat(x)/parseFloat(this.offsetWidth))*100);
+	})
+
+	const tracks = document.querySelectorAll('.audio-track__name');
+
+	tracks.forEach(track => track.addEventListener('mousedown', playTrack));
+	tracks.forEach(tab => tab.addEventListener('touchstart', playTrack));
+
+	function playTrack() {
+		console.log(this.dataset.amplitudeSongIndex);
+		Amplitude.playSongAtIndex(this.dataset.amplitudeSongIndex);
+	}
+
